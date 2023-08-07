ac9fb280663c472f45e9243fde9cd5ecb5974229
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-07 09:14:16 +0330

    update: slave directories

--- test/slave/internal/config/config.go
+++ test/slave/internal/config/config.go
@@ -1,1 +1,71 @@
 package config
+
+import (
+	"encoding/json"
+	"log"
+	"strings"
+
+	"github.com/mehditeymorian/worksync/test/master/internal/db"
+
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/structs"
+	"github.com/tidwall/pretty"
+)
+
+const Prefix = "WS_"
+
+type (
+	Job struct {
+		Name   string `koanf:"name"`
+		Cron   string `koanf:"cron"`
+		MatRun int    `koanf:"max_run"`
+	}
+
+	Config struct {
+		Jobs     []Job     `koanf:"jobs"`
+		Database db.Config `koanf:"database"`
+	}
+)
+
+func New(path string) Config {
+	var instance Config
+
+	k := koanf.New(".")
+
+	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
+		log.Fatalf("error loading default: %s", err)
+	}
+
+	if err := k.Load(file.Provider(path), yaml.Parser()); err != nil {
+		log.Printf("error loading config.yml: %s", err)
+	}
+
+	if err := k.Load(env.Provider(Prefix, ".", func(s string) string {
+		return strings.ReplaceAll(strings.ToLower(
+			strings.TrimPrefix(s, Prefix)), "__", ".")
+	}), nil); err != nil {
+		log.Printf("error loading environment variables: %s", err)
+	}
+
+	if err := k.Unmarshal("", &instance); err != nil {
+		log.Fatalf("error unmarshalling config: %s", err)
+	}
+
+	indent, err := json.MarshalIndent(instance, "", "\t")
+	if err != nil {
+		log.Fatalf("error marshaling config to json: %s", err)
+	}
+
+	indent = pretty.Color(indent, nil)
+	tmpl := `
+	================ Loaded Configuration ================
+	%s
+	======================================================
+	`
+	log.Printf(tmpl, string(indent))
+
+	return instance
+}
